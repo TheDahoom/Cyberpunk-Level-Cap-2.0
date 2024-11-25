7527d130f650bc4bdc347f2235debe30b7cfdbe0
Dahoom <Rozimo56@gmail.com>
2024-11-25 23:08:25 +0300

    reminders v2

--- init.lua
+++ init.lua
@@ -10,10 +10,10 @@ registerForEvent('onInit', function()
     --add description/reminder/modal to restart for changes to take effect
     nativeSettings.addRangeInt("/LevelCap", "Custom level cap", "Choose the max level you can reach", 1, 100, 1, 60, 60, function(value)
         print("Changed level cap to", value)
-        -- TODO
         -- set Proficiencies.Level.maxLevel, LootPrereqs.BelowMaxPlayerLevelPrereq.valueToCheck, LootPrereqs.MaxPlayerLevelPrereq.valueToCheck, LootPrereqs.CyberpsychoWeaponInLootPrereq_end_inline1.valueToCheck, LootPrereqs.LegendaryCWLevelAvailabilityAtVendor_inline1.valueToCheck to value
     end)
 
+    --stop attributes from incremeting after level 79 for OCD
     --add cyberware clamp option for fury
     --test installing mod while at max level
     --test Dont fear the reaper, if it crashes check the since theres probably a .valueToCheck that fails somewhere
