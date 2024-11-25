7206479f80c9c08182fed8a76d13410e8c7f94cf
Dahoom <Rozimo56@gmail.com>
2024-11-25 23:05:21 +0300

    add: tabs and todos/reminders

--- init.lua
+++ init.lua
@@ -1,12 +1,27 @@
--- local nativeSettings = GetMod("nativeSettings")
-local nativeSettings = require("ns")
 local mod = {
     ready = false
 }
 
 print('Test mod')
-nativeSettings.addTab("/cap", "cap")
 registerForEvent('onInit', function()
+    -- local nativeSettings = require("ns") --copied NativeSettings for definitions and autocomplete
+
+    nativeSettings = GetMod("nativeSettings")
+
+
+
+
+    nativeSettings.addTab("/LevelCap", "LevelCap 2.0")
+    --add description/reminder/modal to restart for changes to take effect
+    nativeSettings.addRangeInt("/LevelCap", "Custom level cap", "Choose the max level you can reach", 1, 100, 1, 60, 60, function(value)
+        print("Changed level cap to", value)
+        -- TODO
+        -- set Proficiencies.Level.maxLevel, LootPrereqs.BelowMaxPlayerLevelPrereq.valueToCheck, LootPrereqs.MaxPlayerLevelPrereq.valueToCheck, LootPrereqs.CyberpsychoWeaponInLootPrereq_end_inline1.valueToCheck, LootPrereqs.LegendaryCWLevelAvailabilityAtVendor_inline1.valueToCheck to value
+    end)
+
+    --add cyberware clamp option for fury
+    --test installing mod while at max level
+    --test Dont fear the reaper, if it crashes check the since theres probably a .valueToCheck that fails somewhere
     mod.ready = true
 
     print('mod is inititited')
